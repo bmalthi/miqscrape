322d3e8154267d6f3c7b8c2c14052b6c69f7ed7f
bmalthi <bmalthi@gmail.com>
2021-08-05 23:23:01 +1200

    Open safari when dates

--- local/main.py
+++ local/main.py
@@ -1,24 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 import os
 import datetime
-from urllib.request import Request, urlopen
 
 app = Flask(__name__)
 
-@app.route('/trigger', methods=['GET'])
-def trigger_scrape():
-    print("Starting scrape at:" +str(datetime.datetime.now()))
-    url = 'https://miqscrape-2nqqxkgl5q-ts.a.run.app/scrape'
-    agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0'
-    req = Request(url, headers={'User-Agent': agent})
-    html_bytes = urlopen(req).read()
-    return "Scrapy Scrapy"
-
 @app.route('/', methods=['GET'])
 def receive_message():
+    dates = request.args.get('dates')
     print("Received Message at:" +str(datetime.datetime.now()))
+    print(dates)
+    if dates != 'None':
+        os.system('open -a Safari https://allocation.miq.govt.nz/portal/organisation/5f377e18-43bc-4d0e-a0d3-79be3a2324ec/event/MIQ-DEFAULT-EVENT/accommodation/arrival-date#step-2')
     return "Hello, world!"
-    #os.system('open -a Safari https://allocation.miq.govt.nz/portal/organisation/5f377e18-43bc-4d0e-a0d3-79be3a2324ec/event/MIQ-DEFAULT-EVENT/accommodation/arrival-date#step-2')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
