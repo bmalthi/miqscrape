d7a6bb597b62d72e2c3ff6c245114e960b6519e3
bmalthi <bmalthi@gmail.com>
2021-08-05 16:06:31 +1200

    Remove deadweight

---
+++ readme.txt
@@ -0,0 +1,3 @@
+gcloud source repos clone github_bmalthi_miqscrape /opt/app
+cd /opt/app
+git checkout main
--- teardown.sh
+++
@@ -1,9 +0,0 @@
-set -x
-
-MY_INSTANCE_NAME="my-app-instance-australia-southeast1"
-ZONE=australia-southeast1-a
-
-gcloud compute instances delete $MY_INSTANCE_NAME \
-    --zone=$ZONE --delete-disks=all
-
-gcloud compute firewall-rules delete default-allow-http-8080
