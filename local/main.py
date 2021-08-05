674ddb60e7ba7ef971834505517fe6e9da4169f8
bmalthi <bmalthi@gmail.com>
2021-08-05 23:32:24 +1200

    receive google ip

--- local/main.py
+++ local/main.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def receive_message():
     dates = request.args.get('dates')
-    print("Received Message at:" +str(datetime.datetime.now()))
+    ip_address = request.remote_addr
+    print("Received Message at:" +str(datetime.datetime.now()) + ' from ' + str(ip_address))
     print(dates)
     if dates != 'None':
         os.system('open -a Safari https://allocation.miq.govt.nz/portal/organisation/5f377e18-43bc-4d0e-a0d3-79be3a2324ec/event/MIQ-DEFAULT-EVENT/accommodation/arrival-date#step-2')
