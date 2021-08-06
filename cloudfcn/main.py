fc93723c745214ab465130f608140e47c2bf58e0
bmalthi <bmalthi@gmail.com>
2021-08-06 14:01:38 +1200

    Make local script the master polling cloud

--- cloudfcn/main.py
+++ cloudfcn/main.py
@@ -1,7 +1,7 @@
 from urllib.request import Request, urlopen
 import urllib.parse
 import re
-#from google.cloud import pubsub_v1
+from google.cloud import pubsub_v1
 
 def scrape(request):
     """Scrapes targetted URL, and sends response to local webserver
@@ -9,16 +9,7 @@ def scrape(request):
        +texts user
     """
     ### Get args to determinr user agent and webserver
-    if request.args and 'user_agent' in request.args:
-        raw_agent = request.args.get('user_agent')
-        agent = raw_agent
-    else:
-        agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0'
-    if request.args and 'master_url' in request.args:
-        raw_master_url = request.args.get('master')
-        master_url = raw_master_url
-    else:
-        master_url = 'http://222.153.101.43:9278'
+    agent = request.headers.get('User-Agent')
     ### Send request to MIG
     url = 'https://allocation.miq.govt.nz/portal/'
     req = Request(url, headers={'User-Agent': agent})
@@ -30,13 +21,8 @@ def scrape(request):
     dates_str = ('None' if len(dates) == 0 else str(dates))
     ### Add Pub Sub
     # pushpubsub(dates_str)
-    ### Ping Server with result
-    # Not needed, since server gets response object anyway
-    dates_encode = urllib.parse.quote_plus(dates_str)
-    req = Request(master_url+'?dates='+dates_encode, headers={'User-Agent': agent})
-    urlopen(req).read()
     ### Return result
-    return dates_str
+    return dates_str +'<br>'+agent
 
 def pushpubsub(message):
     project_id = "miqbooking"
--- local/main.py
+++ local/main.py
@@ -1,18 +1,38 @@
-from flask import Flask, request
-import os
 import datetime
+from urllib.request import Request, urlopen
+import urllib.parse
+from random import randint
 
-app = Flask(__name__)
+scrapers = [
+'https://asia-northeast1-miqbooking.cloudfunctions.net/scrape-asia-northeast1',
+'https://asia-southeast1-miqbooking.cloudfunctions.net/scrape-asia-southeast1',
+'https://australia-southeast1-miqbooking.cloudfunctions.net/scrape-australia-southeast1',
+'https://europe-west2-miqbooking.cloudfunctions.net/scrape-europe-west2',
+'https://europe-west3-miqbooking.cloudfunctions.net/scrape-europe-west3',
+'https://europe-west6-miqbooking.cloudfunctions.net/scrape-europe-west6',
+'https://southamerica-east1-miqbooking.cloudfunctions.net/scrape-southamerica-east1',
+'https://us-central1-miqbooking.cloudfunctions.net/scrape-us-central1',
+'https://us-east1-miqbooking.cloudfunctions.net/scrape-us-east1',
+'https://us-west2-miqbooking.cloudfunctions.net/scrape-us-west2']
 
-@app.route('/', methods=['GET'])
-def receive_message():
-    dates = request.args.get('dates')
-    ip_address = request.remote_addr
-    print("Received Message at:" +str(datetime.datetime.now()) + ' from ' + str(ip_address))
-    print(dates)
-    if dates != 'None':
-        os.system('open -a Safari https://allocation.miq.govt.nz/portal/organisation/5f377e18-43bc-4d0e-a0d3-79be3a2324ec/event/MIQ-DEFAULT-EVENT/accommodation/arrival-date#step-2')
-    return "Hello, world!"
+user_agents = [
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15',
+'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1']
+
+def make_request(scraper, agent):
+    req = Request(scraper, headers={'User-Agent': agent})
+    html_bytes = urlopen(req).read()
+    html = html_bytes.decode("utf-8")
+    print(html)
+    #os.system('open -a Safari https://allocation.miq.govt.nz/portal/organisation/5f377e18-43bc-4d0e-a0d3-79be3a2324ec/event/MIQ-DEFAULT-EVENT/accommodation/arrival-date#step-2')
+
+def main():
+    agent = user_agents[randint(0, len(user_agents)-1)]
+    scraper = scrapers[randint(0, len(scrapers)-1)]
+    make_request(scraper, agent)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    main()
