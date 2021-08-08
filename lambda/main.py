71cedae6b8000db63d59d5d95794080556a4b5c1
bmalthi <bmalthi@gmail.com>
2021-08-08 15:45:47 +1200

    push

--- lambda/main.py
+++ lambda/main.py
@@ -1,6 +1,5 @@
 import requests
 import re
-#from google.cloud import pubsub_v1
 
 def scrape(request):
     """Scrapes targetted URL, and sends response to local webserver
@@ -22,17 +21,4 @@ def scrape(request):
     except:
         status_code = 403
         dates_str = None
-    ### Add Pub Sub
-    # pushpubsub(dates_str)
-    ### Return result
     return 'Status:' +str(status_code) +' Dates:' +dates_str
-
-#def pushpubsub(message):
-#    project_id = "miqbooking"
-#    topic_id = "miqdate"
-#    publisher = pubsub_v1.PublisherClient(batch_settings=pubsub_v1.types.BatchSettings(max_messages=1))
-#    topic_path = publisher.topic_path(project_id, topic_id)
-#    # Data must be a bytestring
-#    data = message.encode("utf-8")
-#    # When you publish a message, the client returns a future.
-#    future = publisher.publish(topic_path, data)
